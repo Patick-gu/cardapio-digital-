428eadd29385589de4ff4c3b477f21f1b50dacba
Patrick Gutemberg <84359525+Patick-gu@users.noreply.github.com>
2024-10-22 14:03:16 -0300

    secund

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+let total = 0;
+
+    // Seleciona todos os botões de adicionar item
+    const addItemButtons = document.querySelectorAll('.add-item');
+
+    // Adiciona um evento de clique a cada botão
+    addItemButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        const price = parseFloat(button.getAttribute('data-price'));
+        total += price;
+        document.getElementById('cartTotal').innerText = total.toFixed(2);
+    });
+});
+
+
